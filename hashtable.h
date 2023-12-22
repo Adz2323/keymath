eb741c6618169748ffaa825a75ad99e8f3c4b573
Adz2323 <146026081+Adz2323@users.noreply.github.com>
2023-12-22 16:41:46 +0000

    Update hashtable.h

--- hashtable.h
+++ hashtable.h
@@ -1,38 +1,54 @@
-#ifndef HASHTABLE_H
-#define HASHTABLE_H
+// Copyright 2020 Joshua J Baker. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
 
-#include <stdlib.h>
-#include <string.h>
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
-// Node structure for linked list in each bucket
-struct ListNode
-{
-    char *key;
-    struct ListNode *next;
-};
+struct hashmap;
 
-// Hash table structure
-struct HashTable
-{
-    struct ListNode **buckets;
-    int size;
-    int count;
-};
+struct hashmap *hashmap_new(size_t elsize, size_t cap, uint64_t seed0, 
+    uint64_t seed1, 
+    uint64_t (*hash)(const void *item, uint64_t seed0, uint64_t seed1),
+    int (*compare)(const void *a, const void *b, void *udata),
+    void (*elfree)(void *item),
+    void *udata);
 
-// Function to create a hash table
-struct HashTable *createHashTable(int size);
+struct hashmap *hashmap_new_with_allocator(void *(*malloc)(size_t), 
+    void *(*realloc)(void *, size_t), void (*free)(void*), size_t elsize, 
+    size_t cap, uint64_t seed0, uint64_t seed1,
+    uint64_t (*hash)(const void *item, uint64_t seed0, uint64_t seed1),
+    int (*compare)(const void *a, const void *b, void *udata),
+    void (*elfree)(void *item),
+    void *udata);
 
-// Function to insert a key into the hash table
-void insert(struct HashTable *table, const char *key);
+void hashmap_free(struct hashmap *map);
+void hashmap_clear(struct hashmap *map, bool update_cap);
+size_t hashmap_count(struct hashmap *map);
+bool hashmap_oom(struct hashmap *map);
+const void *hashmap_get(struct hashmap *map, const void *item);
+const void *hashmap_set(struct hashmap *map, const void *item);
+const void *hashmap_delete(struct hashmap *map, const void *item);
+const void *hashmap_probe(struct hashmap *map, uint64_t position);
+bool hashmap_scan(struct hashmap *map, bool (*iter)(const void *item, void *udata), void *udata);
+bool hashmap_iter(struct hashmap *map, size_t *i, void **item);
 
-// Function to search for a key in the hash table
-bool search(struct HashTable *table, const char *key);
+uint64_t hashmap_sip(const void *data, size_t len, uint64_t seed0, uint64_t seed1);
+uint64_t hashmap_murmur(const void *data, size_t len, uint64_t seed0, uint64_t seed1);
+uint64_t hashmap_xxhash3(const void *data, size_t len, uint64_t seed0, uint64_t seed1);
 
-// Function to delete a key from the hash table
-void deleteKey(struct HashTable *table, const char *key);
+const void *hashmap_get_with_hash(struct hashmap *map, const void *key, uint64_t hash);
+const void *hashmap_delete_with_hash(struct hashmap *map, const void *key, uint64_t hash);
+const void *hashmap_set_with_hash(struct hashmap *map, const void *item, uint64_t hash);
+void hashmap_set_grow_by_power(struct hashmap *map, size_t power);
+void hashmap_set_load_factor(struct hashmap *map, double load_factor);
 
-// Function to destroy the hash table and free memory
-void destroyHashTable(struct HashTable *table);
 
-#endif // HASHTABLE_H
+// DEPRECATED: use `hashmap_new_with_allocator`
+void hashmap_set_allocator(void *(*malloc)(size_t), void (*free)(void*));
+
+#endif
